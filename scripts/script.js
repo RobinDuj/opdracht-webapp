504419340a91084e61eb92a8473a9389e394eef7
Robin <robin.dujardyn@student.vives.be>
2024-04-16 14:45:45 +0200

    Toevoegen pagina's

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+async function readNFC() {
+    if (!("NDEFReader" in window)) {
+        alert("Web NFC API is not supported in this browser.");
+        return;
+    }
+
+    const reader = new NDEFReader();
+
+    try {
+        await reader.scan();
+        reader.onreading = event => {
+            document.getElementById('nfc-data').innerText = JSON.stringify(event);
+        };
+    } catch (error) {
+        console.error("Error reading NFC:", error);
+        document.getElementById('nfc-data').innerText = "Error! Cannot read data from the NFC tag. Try a different one?";
+    }
+}
+
+async function writeNFC() {
+    
+}
