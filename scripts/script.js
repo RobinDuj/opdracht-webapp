2f0cb6f58465fd5bd2d5fcdbcd5ed2c408e6b030
Robin <robin.dujardyn@student.vives.be>
2024-04-16 17:33:24 +0200

    kunnen uitlezen en schrijven naar nfc tag

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,12 @@ async function readNFC() {
     try {
         await reader.scan();
         reader.onreading = event => {
-            document.getElementById('nfc-data').innerText = JSON.stringify(event);
+            const records = event.message.records;
+            records.forEach(record => {
+                if (record.recordType === "text") {
+                    readTextRecord(record);
+                }
+            });
         };
     } catch (error) {
         console.error("Error reading NFC:", error);
@@ -22,6 +27,26 @@ async function readNFC() {
     }
 }
 
+function readTextRecord(record) {
+    console.assert(record.recordType === "text");
+    const textDecoder = new TextDecoder(record.encoding);
+    const text = textDecoder.decode(record.data);
+    console.log(`Text: ${text} (${record.lang})`);
+    document.getElementById('nfc-data').innerText = `Text: ${text} (${record.lang})`;
+}
+
 async function writeNFC() {
+    const textToWrite = document.getElementById('nfc-input').value;
     
+    if ("NDEFReader" in window) {
+        const ndef = new NDEFReader();
+        try {
+          await ndef.write(textToWrite);
+          consoleLog("NDEF message written!");
+        } catch(error) {
+          consoleLog(error);
+        }
+      } else {
+        consoleLog("Web NFC is not supported.");
+      }
 }
